004614f44517be95303d0e543c52a617fd27aa3e
ninaadk07 <88612539+ninaadk07@users.noreply.github.com>
2025-07-07 14:22:08 +0100

    Initial Commit

---
+++ Bloomfilter/bloomfilter.py
@@ -0,0 +1,38 @@
+#m = ceil((n * log(p)) / log(1 / pow(2, log(2)))) where n is the number of elements, p is the probability of false positives
+# k = round((m / n) * log(2)) where m is the size of the bit array and n is the number of elements
+class bloomfilter:
+        def __init__(self,size, hash_count):
+                self.size = size
+                self.hash_count = hash_count
+                self.bit_array = [0] * size
+                self.hash_functions = self.generate_hash_functions()
+        def generate_hash_functions(self):
+            hash_fuctions = []
+            for i in range(1,self.hash_count+1):
+                hash_fuctions.append(self.generate_hash_function(i))
+            return hash_fuctions
+        def generate_hash_function(self,seed):
+            def hash_function(value):
+                return hash(value + str(seed)) % self.size
+            return hash_function
+        def add(self, value):
+            for hash_function in self.hash_functions:
+                self.bit_array[hash_function(value)] = 1
+        def contain(self,value):
+            for hash_function in self.hash_functions:
+                if self.bit_array[hash_function(value)] == 0:
+                    return False
+            return True
+
+file = open("./test3-dickens.txt", "r")
+test_file = open("./test-search.txt","r")
+my_bloomfilter = bloomfilter(9161520, 6)
+for line in file:
+    for word in line.split():
+        my_bloomfilter.add(word)
+
+
+for search_word in test_file:
+    search_word = search_word.strip('\n')
+    if not my_bloomfilter.contain(search_word):
+        print(search_word + " Not Found")
